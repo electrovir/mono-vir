a809796dfc9991d7532429231dc7d234a13a9819
electrovir <electrovir@users.noreply.github.com>
2025-08-17 13:54:45 +0000

    [dev] remove unpredictable snapshot test

--- src/cli/cli.test.ts
+++ src/cli/cli.test.ts
@@ -1,9 +1,7 @@
-import {type ArrayElement, mapObjectValues, omitObjectKeys} from '@augment-vir/common';
 import {type ShellOutput} from '@augment-vir/node';
-import {describe, snapshotCases} from '@augment-vir/test';
+import {describe, itCases} from '@augment-vir/test';
 import {runPackageCli} from 'test-as-package';
 import {testRepos} from '../file-paths.mock.js';
-import {sanitizeTestOutput} from '../sanitize-output.mock.js';
 
 describe('cli', () => {
     const outputKeysToIgnore = [
@@ -11,30 +9,14 @@ describe('cli', () => {
         'exitSignal',
     ] as const satisfies ReadonlyArray<keyof ShellOutput>;
 
-    snapshotCases(
-        async (
-            cwd: string,
-            command: string,
-            trimKeys: (keyof Omit<ShellOutput, ArrayElement<typeof outputKeysToIgnore>>)[] = [],
-        ) => {
+    itCases(
+        async (cwd: string, command: string) => {
             const finalArgs = command.split(' ');
             const output = await runPackageCli({
                 cwd,
                 commandArgs: finalArgs,
             });
-            return mapObjectValues(
-                omitObjectKeys(
-                    {
-                        ...output,
-                        args: finalArgs.join(' '),
-                    },
-                    [
-                        ...outputKeysToIgnore,
-                        ...trimKeys,
-                    ],
-                ),
-                (key, value) => sanitizeTestOutput(value),
-            );
+            return output.exitCode;
         },
         [
             /**
@@ -50,6 +32,7 @@ describe('cli', () => {
                     testRepos['augment-vir'],
                     'for-each npm run --silent mono-vir-test:success',
                 ],
+                expect: 0,
             },
             {
                 it: 'for-each: errors if one of the scripts fails',
@@ -57,6 +40,7 @@ describe('cli', () => {
                     testRepos['augment-vir'],
                     'for-each npm run --silent mono-vir-test:one-failure',
                 ],
+                expect: 1,
             },
             {
                 it: 'for-each: errors if the given command is invalid',
@@ -64,6 +48,7 @@ describe('cli', () => {
                     testRepos['augment-vir'],
                     'fake-command npm run --silent mono-vir-test:success',
                 ],
+                expect: 1,
             },
             {
                 it: 'for-each: errors if no inputs are given to for-each',
@@ -71,6 +56,7 @@ describe('cli', () => {
                     testRepos['augment-vir'],
                     'for-each',
                 ],
+                expect: 1,
             },
             /**
              * =========================
@@ -84,16 +70,16 @@ describe('cli', () => {
                 inputs: [
                     testRepos['augment-vir'],
                     'for-each-async npm run --silent mono-vir-test:success',
-                    ['stdout'],
                 ],
+                expect: 0,
             },
             {
                 it: 'for-each-async: errors if one of the scripts fails',
                 inputs: [
                     testRepos['augment-vir'],
                     'for-each-async npm run --silent mono-vir-test:one-failure',
-                    ['stdout'],
                 ],
+                expect: 1,
             },
             {
                 it: 'for-each-async: errors if the given command is invalid',
@@ -101,6 +87,7 @@ describe('cli', () => {
                     testRepos['augment-vir'],
                     'fake-command npm run --silent mono-vir-test:success',
                 ],
+                expect: 1,
             },
             {
                 it: 'for-each-async: errors if no inputs are given to for-each-async',
@@ -108,6 +95,7 @@ describe('cli', () => {
                     testRepos['augment-vir'],
                     'for-each-async',
                 ],
+                expect: 1,
             },
         ],
     );
